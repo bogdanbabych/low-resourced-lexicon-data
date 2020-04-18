32c7cec71b4511b73fd233d15b3c85455050310e
Bogdan Babych <bogdan.babych@gmail.com>
2020-04-18 13:27:59 +0100

    s010x development

---
+++ src/s1010hy-wiki/s1010hy-wiki2text-v01.py
@@ -0,0 +1,17 @@
+'''
+Created on 25 Mar 2020
+
+@author: bogdan
+'''
+
+class s1010hy_wiki2text(object):
+    '''
+    parsing wikipedia xml, extracting textual input
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ src/s1010hy-wiki/s1010hy-wiki2text-v01.sh
@@ -0,0 +1,1 @@
+python s1010hy-wiki2text-v01.py hywiki-20170601-pages-articles-tail2k.xml >s1010hy-wiki2text-v01-out.txt
